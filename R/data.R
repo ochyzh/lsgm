8fb88f756eb6a95b7f3544f71b1c80e4e8bd1907
Olga Chyzh <ochyzh@iastate.edu>
2024-10-02 13:22:38 -0400

    Update data.R

--- R/data.R
+++ R/data.R
@@ -1,11 +1,11 @@
 #' Toy spatial data
 #'
-#' A list containing observations for two variables and a connectivity matrix
+#' A dataset containing observations for two variables and a connectivity matrix
 #'
-#' @format A list containing 100 rows and 2 variables:
+#' @format A dataset containing 100 observations for 2 variables:
 #' \describe{
-#'   \item{X}{a real-valued vector of dimension 100x1}
-#'   \item{Y}{a binary-valued vector of dimension 100x1}
+#'   \item{X}{a real-valued variable of dimension 100x1}
+#'   \item{Y}{a binary-valued variable of dimension 100x1}
 #'   \item{W}{a real-valued matrix of dimension 100x100}
 #' }
 "toy_data"
